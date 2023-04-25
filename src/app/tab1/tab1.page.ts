0e241e81374c28769209ae142a04fba079cf3533
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2023-04-25 11:13:37 -0300

    Add files via upload

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { IFilme } from '../model/IFilme';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+  listaFilmes: IFilme[] = [
+    {
+      nome: 'Mortal Kombat (2021)',
+      lancamento: '15/04/2021',
+      duracao: '1h50m',
+      classificacao: 9,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/w8BVC3qrCWCiTHRz4Rft12dtQF0.jpg',
+      generos: ['Ação', 'Fantasia', 'Aventura'],
+      pagina: '/mortal-kombat',
+      favorito: false
+    },
+    {
+      nome: 'Vingadores: Ultimato (2019)',
+      lancamento: '25/04/2019 (BR)',
+      duracao: '3h01m',
+      classificacao: 6,
+      cartaz: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/q6725aR8Zs4IwGMXzZT8aC8lh41.jpg',
+      generos: ['Aventura', 'Ficção científica', 'Ação'],
+      pagina: '/avengers',
+      favorito: false
+    }
+  ];
+
+}
